d99fae55cb072f161e4e8502d7f8ea6f241757c6
Homer Haddock <snakevs007NOTCLICKBAIT@gmail.com>
2023-11-28 23:52:38 -0700

    Added CLI

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import argparse
 import logic
+import inputapi as inp
 
 parser = argparse.ArgumentParser(
     prog="9 Divider",
     description="Divides an integer by nine without using x / 9",
 )
-parser.add_argument("number", type=int, help="The number to divide by nine")
-parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument(
+    "number",
+    type=int,
+    nargs="?",
+    default="0",
+    help="The number to divide by nine, enter nothing to use CLI",
+)
+parser.add_argument("-v", "--verbose", action="store_true")
 args = parser.parse_args()
+
+if args.number == 0:
+    args.number = inp.numerical.integer.newLineInt("Number to divide:", allowNeg=False)
+elif args.number < 0:
+    raise ValueError("Number must be positive")
+
 print(args)
-print(logic.nearestValidNumber(args.number, args.verbose))
+print(logic.divideWholeByNine(args.number, args.verbose))
