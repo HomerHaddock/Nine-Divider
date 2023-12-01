42b230df7b28b419c57375ecf93a8945e34630ad
Homer Haddock <snakevs007NOTCLICKBAIT@gmail.com>
2023-12-01 09:42:38 -0700

    Added cache logic

---
+++ src/cache.py
@@ -0,0 +1,31 @@
+import json
+from typing import Tuple
+import os
+
+cachePath = os.path.dirname(os.path.realpath(__file__)) + "/.cache"
+
+
+class Cache:
+    def __init__(self, values: dict):
+        self.values = values
+
+    def loadValue(self, value: int) -> Tuple[int, int] | False:
+        if value not in self.values:
+            return False
+        return self.values[value]
+
+    def saveValue(self, value: int, result: Tuple[int, int]) -> None:
+        if not isinstance(value, tuple):
+            raise TypeError("Result is not a tuple")
+        if not len(result) == 2:
+            raise ValueError("Result must have two values")
+        self.values[value] = result
+
+    def saveCache(self) -> None:
+        with open(cachePath, "w") as cache:
+            json.dump(self.values, cache, indent=4, sort_keys=True)
+
+
+def loadCache() -> Cache:
+    with open(cachePath, "r") as cache:
+        return Cache(json.load(cache))
