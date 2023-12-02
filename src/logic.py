c598d940642861e9943c7f2c2c8e64ec1bd07e53
Homer Haddock <snakevs007NOTCLICKBAIT@gmail.com>
2023-12-02 04:21:38 -0700

    Modified formatResults Added cache logic Fixed bugs

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from cache import loadCache
 
 
 def splitDigits(number: int) -> List[int]:
@@ -82,6 +83,8 @@ def divideWholeByNine(number: int, verbose: bool = False) -> int:
     Returns:
         int: The divided number
     """
+    if number == 0:
+        return 0
     subtraction = numberToSubtractFrom(number, verbose)
     if verbose:
         print("Dividing valid number")
@@ -89,23 +92,45 @@ def divideWholeByNine(number: int, verbose: bool = False) -> int:
     return subtraction - digits[-1]
 
 
-def divideNumberByNine(number: int, verbose: bool = False) -> Tuple[int, int]:
+def divideNumberByNine(
+    number: int, verbose: bool = False, skipCache: bool = False
+) -> Tuple[int, int]:
     """
     divideNumberByNine divides any whole integer by nine and gives a decimal
 
     Args:
         number (int): The number to divide by nine (Can be any whole number)
         verbose (bool, optional): Wether or not the output should be verbose. Defaults to False.
+        skipCache (bool, optional): Skips both loading and saving to cache. Defaults to False.
 
     Returns:
         Tuple[int, int]: The number divided by nine (Whole number, Decimal (repeating))
     """
+    if not skipCache:
+        if verbose:
+            print("Checking cache")
+        # Return result if in cache
+        value = cache.loadValue(str(number))
+        if value:
+            if verbose:
+                print("Answer found in cache")
+            return value
+
     if verbose:
         print("Dividing by nine")
         print("Found decimal")
+
+    # Divide number by nine
     valid, decimal = nearestValidNumber(number, verbose)
     dividedWhole = divideWholeByNine(valid, verbose)
-    return dividedWhole, decimal
+    result = (dividedWhole, decimal)
+
+    if not skipCache:
+        # Save result to cache
+        cache.saveValue(str(number), result)
+        cache.saveCache()
+
+    return result
 
 
 def formatResults(result: Tuple[int, int], verbose: bool = False) -> str:
@@ -113,12 +138,17 @@ def formatResults(result: Tuple[int, int], verbose: bool = False) -> str:
     formatResults formats the divideNumberByNine into a readable string
 
     Args:
-        result (Tuple[int, int]): The result
+        result (Tuple[int, int]): The result to format
         verbose (bool, optional): Wether or not the output should be verbose. Defaults to False.
 
     Returns:
-        str: The result formatted to "$WHOLE.($DECIMAL)"
+        str: The result formatted to "$WHOLE.($DECIMAL)" or just "$WHOLE" if decimal is 0
     """
     if verbose:
         print("Formatting result")
+    if result[1] == 0:
+        return str(result[0])
     return "%s.(%s)" % result
+
+
+cache = loadCache()
